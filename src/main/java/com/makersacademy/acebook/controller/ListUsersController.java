ce9c77b19cddc8888abf0fd7ca1419599af8d429
Tom <T.A.Rose1993@gmail.com>
2022-06-30 16:42:36 +0100

    Not working add friends, cannot get the list users to display any users

---
+++ src/main/java/com/makersacademy/acebook/controller/ListUsersController.java
@@ -0,0 +1,32 @@
+package com.makersacademy.acebook.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+import com.makersacademy.acebook.repository.UserRepository;
+
+import com.makersacademy.acebook.model.User;
+
+@Controller
+public class ListUsersController {
+
+  @Autowired
+    UserRepository userRepository;
+  
+  @GetMapping("/listUsers")
+  public String user(Model model){
+    Iterable<User> users;
+    Authentication loggedIn = SecurityContextHolder.getContext().getAuthentication();
+    User user = userRepository.findByUsername(loggedIn.getName());
+    model.addAttribute("user", user);
+    return "/users/listUsers";
+  }
+}
